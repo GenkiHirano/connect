d850584b93f7fb009f542515b47242659127a1c2
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-19 21:40:30 +0900

    タイトルのテストを作成
    タイトル表示の保守性を担保する為

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#full_title' do
+    it "full_titleの引数が空の場合、タイトルは#{Const::BASE_TITLE}'と等しい" do
+      expect(full_title("")).to eq Const::BASE_TITLE
+    end
+
+    it "full_titleの引数がnilの場合、タイトルは#{Const::BASE_TITLE}'と等しい" do
+      expect(full_title(nil)).to eq Const::BASE_TITLE
+    end
+
+    it "full_titleの引数がtestの場合、タイトルは'test - #{Const::BASE_TITLE}'と等しい" do
+      expect(full_title("test")).to eq "test - #{Const::BASE_TITLE}"
+    end
+  end
+end
+  
