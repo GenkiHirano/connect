c03415d1ea720feffb5bf5194bf6d3a1b73f90c0
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-23 18:54:25 +0900

    rails g migration add_password_digest_to_users password_digest:stringコマンドにてUserモデルにPWカラムを追加、DBに反映
    UserモデルにPWカラムを追加する為

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_093955) do
+ActiveRecord::Schema.define(version: 2020_11_23_095252) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_093955) do
     t.string "sex"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
