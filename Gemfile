68bfabcf41136b4fd0939defcd94a9e9aa7a3b19
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-23 19:01:37 +0900

    bcryptをbundle install
    has_secure_passwordを使ってパスワードをハッシュ化する為

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
-gem 'rails',      '~> 6.0.3', '>= 6.0.3.3'
+gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
+gem 'bcrypt'
 gem 'puma',       '~> 4.1'
 gem 'sass-rails', '>= 6'
 gem 'webpacker',  '~> 4.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_11_23_095252) do
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -22,5 +21,4 @@ ActiveRecord::Schema.define(version: 2020_11_23_095252) do
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
