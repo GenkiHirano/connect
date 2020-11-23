fa2451e805befda036d7cb0bff31bc1b649e2143
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-24 00:55:14 +0900

    spec/factories/users.rbファイルを作成、ファクトリを作成
    Rspecでファクトリを使用する為

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    sequence(:email) { |n| "example#{n}@example.com" }
+    password { "foobar" }
+    password_confirmation { "foobar" }
+    introduction { "はじめまして。米津玄師が好きです！" }
+    sex { "男性" }
+  end
+end
