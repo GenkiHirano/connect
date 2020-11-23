87c452ac4dd67284613d9f311d56e66698a28404
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-23 18:46:01 +0900

    マイグレーションファイルにunique: trueを追加、DBに反映
    メールアドレスの一意性を保たれる様、設定する為

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_091847) do
+ActiveRecord::Schema.define(version: 2020_11_23_093955) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,5 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_091847) do
     t.string "sex"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
