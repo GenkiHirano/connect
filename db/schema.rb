13183420b6d05ff930a356fc32b99b590dc562ba
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-23 18:39:11 +0900

    Userモデルにバリデーション、beforeアクションを設定
    安全性を向上させる為、ドライなコードにする為

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_11_23_091847) do
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -20,5 +19,4 @@ ActiveRecord::Schema.define(version: 2020_11_23_091847) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
