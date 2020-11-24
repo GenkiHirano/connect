6119fe3d6f45ba3f26955658c03bd90012e204b6
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-24 13:42:55 +0900

    sessionsコントローラーのルーティング作成
    ログイン機能を実装する為

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root 'static_pages#top'
-  resources      :users
-  get :signup, to: 'users#new'
-  get :about,  to: 'static_pages#about'
+  resources         :users
+  get    :signup, to: 'users#new'
+  get    :about,  to: 'static_pages#about'
+  get    :login,  to: 'sessions#new'
+  post   :login,  to: 'sessions#create'
+  delete :logout, to: 'sessions#destroy'
 end
