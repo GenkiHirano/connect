32e663218da86801f864cc4a9caddd7dc0f11c50
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-26 00:02:42 +0900

    ヘッダーからユーザー編集リンクを削除
    ユーザー一覧表示機能を実装する為

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,12 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def index
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def edit
