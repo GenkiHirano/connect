de0477a6564747a4843cfa27a2df658ae2adba88
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-27 22:54:55 +0900

    マイグレーションファイルを修正、DBに反映
    ライブ同行者投稿機能を実装する為

--- db/migrate/20201127134752_create_live_companions.rb
+++ db/migrate/20201127134752_create_live_companions.rb
@@ -1,14 +1,14 @@
 class CreateLiveCompanions < ActiveRecord::Migration[6.0]
   def change
     create_table :live_companions do |t|
-      t.integer :user_id
       t.string :artist_name
       t.string :live_name
       t.date :schedule
       t.text :recruitment
-      t.references :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
+    add_index :live_companions, [:user_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_064945) do
+ActiveRecord::Schema.define(version: 2020_11_27_134752) do
+
+  create_table "live_companions", force: :cascade do |t|
+    t.string "artist_name"
+    t.string "live_name"
+    t.date "schedule"
+    t.text "recruitment"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_live_companions_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_live_companions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -23,4 +36,6 @@ ActiveRecord::Schema.define(version: 2020_11_27_064945) do
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "live_companions", "users"
 end
