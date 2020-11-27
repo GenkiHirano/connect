ddafc918d2e439b292a6bb2cc914fb4d267d5307
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-28 00:53:27 +0900

    app/models/live_companion.rb追記
    バリデーションをテストする為

--- spec/models/live_companion_spec.rb
+++ spec/models/live_companion_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe LiveCompanion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let!(:live_companion) { create(:live_companion) }
+
+  context "バリデーション" do
+    it "有効な状態であること" do
+      expect(live_companion).to be_valid
+    end
+
+    it "アーティスト名がなければ無効な状態であること" do
+      live_companion = build(:live_companion, artist_name: nil)
+      live_companion.valid?
+      expect(live_companion.errors[:artist_name]).to include("を入力してください")
+    end
+
+    it "ライブ名がなければ無効な状態であること" do
+      live_companion = build(:live_companion, live_name: nil)
+      live_companion.valid?
+      expect(live_companion.errors[:live_name]).to include("を入力してください")
+    end
+
+    it "アーティスト名が30文字以内であること" do
+      live_companion = build(:live_companion, artist_name: "あ" * 31)
+      live_companion.valid?
+      expect(live_companion.errors[:artist_name]).to include("は30文字以内で入力してください")
+    end
+
+    it "ライブ名が30文字以内であること" do
+      live_companion = build(:live_companion, live_name: "あ" * 31)
+      live_companion.valid?
+      expect(live_companion.errors[:live_name]).to include("は30文字以内で入力してください")
+    end
+
+    it "ライブメモが140文字以内であること" do
+      live_companion = build(:live_companion, live_memo: "あ" * 141)
+      live_companion.valid?
+      expect(live_companion.errors[:live_memo]).to include("は140文字以内で入力してください")
+    end
+
+    it "ユーザーIDがなければ無効な状態であること" do
+      live_companion = build(:live_companion, user_id: nil)
+      live_companion.valid?
+      expect(live_companion.errors[:user_id]).to include("を入力してください")
+    end
+  end
 end
