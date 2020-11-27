439e47dc6bb057b3778ecbf6b76edb3ce2c88c10
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-28 00:31:30 +0900

    app/models/live_companion.rb追記
    バリデーションを設定する為

--- app/models/live_companion.rb
+++ app/models/live_companion.rb
@@ -1,3 +1,7 @@
 class LiveCompanion < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :artist_name, presence: true, length: { maximum: 30 }
+  validates :live_name, presence: true, length: { maximum: 30 }
+  validates :live_memo, length: { maximum: 140 }
 end
