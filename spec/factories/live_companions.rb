466f7aeaf7450d1ba2e9897c97e8aa892179227d
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-28 00:58:58 +0900

    spec/factories/live_companion.rb追記
    ファクトリに作成日時が異なる設定を追加する為

--- spec/factories/live_companions.rb
+++ spec/factories/live_companions.rb
@@ -5,5 +5,18 @@ FactoryBot.define do
     schedule { "2021-8-6" }
     live_memo { "米民さん、誰か一緒にライブ行きませんか...？" }
     association :user
+    created_at { Time.current } 
+  end
+
+  trait :yesterday do
+    created_at { 1.day.ago }
+  end
+
+  trait :one_week_ago do
+    created_at { 1.week.ago }
+  end
+
+  trait :one_month_ago do
+    created_at { 1.month.ago }
   end
 end
