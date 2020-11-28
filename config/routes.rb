7c242229f41ca17b750415c8b5b68a360e6d3735
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-29 00:41:25 +0900

    ルーティングにresources :live_companionsを追記
    投稿機能を実装する為

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root 'static_pages#top'
   resources         :users
+  resources         :live_companions
   get    :signup, to: 'users#new'
   get    :about,  to: 'static_pages#about'
   get    :login,  to: 'sessions#new'
