c44a62bca7ebc38556d6e78beff630f5f631ff0a
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-28 22:08:52 +0900

    app/views/static_pages/top.html.erb修正、liveパーシャル作成
    topページに投稿の一覧を表示できる様にする為

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def top
-    @live = current_user.live.paginate(page: params[:page], per_page: 5)
+    @live_items = current_user.live.paginate(page: params[:page], per_page: 5)
   end
 
   def about
