571b42ad88cfe0773da518905b689d1114dc7298
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-29 01:32:28 +0900

    topページに投稿機能へのリンクが存在するかのテストを追記
    topページに投稿機能へのリンクを追加した為

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -18,6 +18,10 @@ RSpec.describe "StaticPages", type: :system do
       let!(:user) { create(:user) }
       let!(:live_companion) { create(:live_companion, user: user) }
 
+      before do
+        login_for_system(user)
+      end
+
       it "ライブ同行者投稿のぺージネーションが表示されること" do
         login_for_system(user)
         create_list(:live_companion, 6, user: user)
@@ -27,6 +31,11 @@ RSpec.describe "StaticPages", type: :system do
         LiveCompanion.take(5).each do |d|
           expect(page).to have_link d.artist_name
         end
+      end
+
+      it "「一緒にライブに行く人を募集する」リンクが表示されること" do
+        visit root_path
+        expect(page).to have_link "一緒にライブに行く人を募集する", href: new_live_companion_path
       end
     end
   end
