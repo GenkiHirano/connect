bdaa9edae3ac89e49ac047502d59b55674f850b7
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-29 22:49:29 +0900

    投稿機能のリクエストテスト追記
    別アカウントのユーザからの編集はホーム画面にリダイレクトされることをテストする為

--- spec/requests/live_companions_edit_spec.rb
+++ spec/requests/live_companions_edit_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
-RSpec.describe "料理編集", type: :request do
+RSpec.describe "投稿編集", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
   let!(:live_companion) { create(:live_companion, user: user) }
 
   context "認可されたユーザーの場合" do
@@ -32,6 +33,22 @@ RSpec.describe "料理編集", type: :request do
       } }
       expect(response).to have_http_status "302"
       expect(response).to redirect_to login_path
+    end
+  end
+
+  context "別アカウントのユーザーの場合" do
+    it "ホーム画面にリダイレクトすること" do
+      login_for_request(other_user)
+      get edit_live_companion_path(live_companion)
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
+      patch live_companion_path(live_companion), params: { live_companion: {
+        artist_name: "米津玄師",
+        live_name: "米津玄師 2020 TOUR / HYPE",
+        live_memo: "誰か、米津玄師さんの一緒にライブ行きませんか...？"
+      } }
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
     end
   end
 end
