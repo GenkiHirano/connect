c9d40d511eb2869253057276c414542c92ad9f70
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-11-30 22:06:13 +0900

    投稿削除機能のシステムテストを実装
    投稿削除機能の保守性を担保し、投稿削除後、フラッシュが表示されることをテストする為

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -37,6 +37,13 @@ RSpec.describe "StaticPages", type: :system do
         visit root_path
         expect(page).to have_link "一緒にライブに行く人を募集する", href: new_live_companion_path
       end
+
+      it "投稿を削除後、削除成功のフラッシュが表示されること" do
+        visit root_path
+        click_on '削除'
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content '投稿が削除されました'
+      end
     end
   end
 
