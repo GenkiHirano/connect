2889d9b8dbf6c3c95e004f96785268c5693360d0
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-01 21:40:41 +0900

    FavoritesControllerのcreate、destroyアクションに追記
    お気に入りの登録/解除機能を実装する為

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @live_companion = LiveCompanion.find(params[:live_companion_id])
+    @user = @live_companion.user
+    current_user.favorite(@dish)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    @live_companion = LiveCompanion.find(params[:live_companion_id])
+    current_user.favorites.find_by(live_companion_id: @live_companion.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Users", type: :system do
   let!(:user) { create(:user) }
   let!(:admin_user) { create(:user, :admin) }
   let!(:other_user) { create(:user) }
+  let!(:live_companion) { create(:live_companion, user: user) }
 
   describe "ユーザー一覧ページ" do
     context "管理者ユーザーの場合" do
@@ -167,6 +168,20 @@ RSpec.describe "Users", type: :system do
         expect(page).to have_button 'フォロー中'
         click_button 'フォロー中'
         expect(page).to have_button 'フォローする'
+      end
+    end
+
+    context "お気に入り登録/解除" do
+      before do
+        login_for_system(user)
+      end
+
+      it "ライブ同行者募集投稿のお気に入り登録/解除ができること" do
+        expect(user.favorite?(live_companion)).to be_falsey
+        user.favorite(live_companion)
+        expect(user.favorite?(live_companion)).to be_truthy
+        user.unfavorite(live_companion)
+        expect(user.favorite?(live_companion)).to be_falsey
       end
     end
   end
