48a832ba818254f39699ad7d8a19e07e5ffaa41e
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-01 21:14:12 +0900

    LiveCompanionモデルにhas_many :favorites, dependent: :destroy追記
    userとlive_companionを関連付けし、一緒に削除される様にする為

--- app/models/live_companion.rb
+++ app/models/live_companion.rb
@@ -1,5 +1,6 @@
 class LiveCompanion < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :artist_name, presence: true, length: { maximum: 30 }
