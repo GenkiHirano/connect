a01ca0cdf073ef0db44c5e4cdf347b7554dc7355
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-02 01:11:38 +0900

    Favoriteモデルにdefault_scope -> { order(created_at: :desc) }を追記
    お気に入りした日付が新しい投稿から並べるよう実装する為

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :live_companion
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :live_companion_id, presence: true
 end
