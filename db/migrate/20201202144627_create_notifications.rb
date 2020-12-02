7367299363daaeb54a5a8214bf69d3a4a701fd48
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-02 23:47:21 +0900

    通知機能のNotificationモデルを作成
    通知機能を実装する為

---
+++ db/migrate/20201202144627_create_notifications.rb
@@ -0,0 +1,13 @@
+class CreateNotifications < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notifications do |t|
+      t.integer :user_id
+      t.integer :live_companion_id
+      t.integer :variety
+      t.text :content
+      t.integer :from_user_id
+
+      t.timestamps
+    end
+  end
+end
