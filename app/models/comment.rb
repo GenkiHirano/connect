d45361033cdda6b7e46c386fed96e38a40522f71
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-02 13:42:32 +0900

    Commentモデルに関連付け、バリデーションを追記
    存在性、一貫性、関連付けを実装する為

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :live_companion
+  validates :user_id, presence: true
+  validates :live_companion_id, presence: true
+  validates :content, presence: true, length: { maximum: 50 }
 end
