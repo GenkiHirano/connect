7ea2e86b0ea869788ec25063f79927368fe04f2b
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-05 22:30:57 +0900

    LiveCompanionモデル追記
    CarrierWave gemに画像と関連付けたモデルを伝える為

--- app/models/live_companion.rb
+++ app/models/live_companion.rb
@@ -3,6 +3,7 @@ class LiveCompanion < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :artist_name, presence: true, length: { maximum: 30 }
   validates :live_name, presence: true, length: { maximum: 30 }
