5b4c08ec772d477ce0d6a4fdb3ec02e2001d18ed
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-05 22:52:32 +0900

    LiveCompanionsControllerのlive_companion_paramsメソッド追記
    画像投稿機能を実装する為

--- app/controllers/live_companions_controller.rb
+++ app/controllers/live_companions_controller.rb
@@ -50,7 +50,7 @@ class LiveCompanionsController < ApplicationController
   private
 
     def live_companion_params
-      params.require(:live_companion).permit(:artist_name, :live_name, :live_memo)
+      params.require(:live_companion).permit(:artist_name, :live_name, :live_memo, :picture)
     end
 
     def correct_user
