aab72dfcaa8e774189996dd7340b946e1a684cff
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-06 01:31:03 +0900

    ライブ同行者を募集する(投稿)リクエストテストにpicture属性を追加
    投稿の保守性を担保する為

--- spec/requests/live_companions_record_spec.rb
+++ spec/requests/live_companions_record_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe "ライブ同行者を募集する(投稿)", type: :request do
   let!(:user) { create(:user) }
   let!(:live_companion) { create(:live_companion, user: user) }
+  let(:picture_path) { File.join(Rails.root, 'spec/fixtures/test_live_companion.jpg') }
+  let(:picture) { Rack::Test::UploadedFile.new(picture_path) }
+
 
   context "ログインしているユーザーの場合" do
     before do
@@ -21,7 +24,8 @@ RSpec.describe "ライブ同行者を募集する(投稿)", type: :request do
         post live_companions_path, params: {
           live_companion: { artist_name: "米津玄師",
                             live_name: "米津玄師 2020 TOUR / HYPE",
-                            live_memo: "誰か、米津玄師さんの一緒にライブ行きませんか...？" }
+                            live_memo: "誰か、米津玄師さんの一緒にライブ行きませんか...？",
+                            picture: picture  }
         }
       }.to change(LiveCompanion, :count).by(1)
       follow_redirect!
@@ -33,7 +37,8 @@ RSpec.describe "ライブ同行者を募集する(投稿)", type: :request do
         post live_companions_path, params: {
           live_companion: { artist_name: "",
                             live_name: "米津玄師 2020 TOUR / HYPE",
-                            live_memo: "誰か、米津玄師さんの一緒にライブ行きませんか...？" }
+                            live_memo: "誰か、米津玄師さんの一緒にライブ行きませんか...？",
+                            picture: picture  }
         }
       }.not_to change(LiveCompanion, :count)
       expect(response).to render_template('live_companions/new')
