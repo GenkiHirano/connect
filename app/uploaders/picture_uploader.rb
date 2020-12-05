90993bdd139574811c55567aaf40cb8a06efc522
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-05 23:42:26 +0900

    picture_uploader.rb追記
    ページによって適切な画像サイズにリサイズし、画像が未設定の場合はデフォルトで設定した画像を設定する為

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,8 +1,18 @@
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   storage :file
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url(*args)
+    "/images/" + [version_name, "default.png"].compact.join('_')
+  end
+
+  version :thumb400 do
+    process resize_and_pad(400, 400, background = :transparent, gravity = 'Center')
   end
 
   def extension_whitelist
