3f27f1e94e681d4657cc5bf4ec8ff5fe094b6919
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-06 01:39:59 +0900

    投稿編集機能のリクエストテストにpicture属性を追加
    投稿編集機能の保守性を担保する為

--- spec/requests/live_companions_edit_spec.rb
+++ spec/requests/live_companions_edit_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "投稿編集", type: :request do
   let!(:user) { create(:user) }
   let!(:other_user) { create(:user) }
   let!(:live_companion) { create(:live_companion, user: user) }
+  let(:picture2_path) { File.join(Rails.root, 'spec/fixtures/test_live_companion2.jpg') }
+  let(:picture2) { Rack::Test::UploadedFile.new(picture2_path) } 
 
   context "認可されたユーザーの場合" do
     it "レスポンスが正常に表示されること(+フレンドリーフォワーディング)" do
@@ -13,7 +15,8 @@ RSpec.describe "投稿編集", type: :request do
       patch live_companion_path(live_companion), params: { live_companion: {
         artist_name: "米津玄師",
         live_name: "米津玄師 2020 TOUR / HYPE",
-        live_memo: "誰か、米津玄師さんの一緒にライブ行きませんか...？"
+        live_memo: "誰か、米津玄師さんの一緒にライブ行きませんか...？",
+        picture: picture2
       } }
       redirect_to live_companion
       follow_redirect!
