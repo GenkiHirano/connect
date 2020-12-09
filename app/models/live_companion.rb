3848f8a9c806d16f77191defefb00e3e4e2248c0
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-09 15:43:27 +0900

    ライブ日程の存在性のバリデーションを追記
    ライブ日程が空の場合は投稿できない様にする為

--- app/models/live_companion.rb
+++ app/models/live_companion.rb
@@ -7,6 +7,7 @@ class LiveCompanion < ApplicationRecord
   validates :user_id, presence: true
   validates :artist_name, presence: true, length: { maximum: 30 }
   validates :live_name, presence: true, length: { maximum: 30 }
+  validates :schedule, presence: true
   validates :live_memo, length: { maximum: 140 }
 
   def feed_comment(live_companion_id)
