b2603fcfae97eb411f3e4254738bdc82c7e30f10
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-10 15:56:19 +0900

    LiveListモデルのバリデーションテストを作成
    LiveListモデルのバリデーションが正常に動作しているかテストする為

--- spec/models/live_list_spec.rb
+++ spec/models/live_list_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe LiveList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:live_list) { create(:live_list) }
+
+  context "バリデーション" do
+    it "live_listインスタンスが有効であること" do
+      expect(live_list).to be_valid
+    end
+
+    it "user_idがnilの場合、無効であること" do
+      live_list.user_id = nil
+      expect(live_list).not_to be_valid
+    end
+
+    it "live_companion_idがnilの場合、無効であること" do
+      live_list.live_companion_id = nil
+      expect(live_list).not_to be_valid
+    end
+
+    it "from_user_idがnilの場合、無効であること" do
+      live_list.from_user_id = nil
+      expect(live_list).not_to be_valid
+    end
+  end
 end
