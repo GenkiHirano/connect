cf78657949f5a4f114badedf68cfb268f5a55435
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-10 15:39:19 +0900

    LiveListモデルにuserとlive_companionを関連付け、バリデーション、投稿の表示順を設定
    ライブ予定リストを実装するために関連付けする為

--- app/models/live_list.rb
+++ app/models/live_list.rb
@@ -1,2 +1,8 @@
 class LiveList < ApplicationRecord
+  belongs_to :user
+  belongs_to :live_companion
+  default_scope -> { order(schedule: :asc) }
+  validates :user_id, presence: true
+  validates :live_companion_id, presence: true
+  validates :from_user_id, presence: true
 end
