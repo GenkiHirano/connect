51a2220cb084dbabef4986772bbc3982c5b37283
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-10 16:51:17 +0900

    LiveListsControllerのcreate、destroyアクションを追記
    ライブ予定リストの登録、削除機能を実装する為

--- app/controllers/live_lists_controller.rb
+++ app/controllers/live_lists_controller.rb
@@ -5,8 +5,22 @@ class LiveListsController < ApplicationController
   end
 
   def create
+    @live_companion = LiveCompanion.find(params[:live_companion_id])
+    @user = @live_companion.user
+    current_user.live_list(@live_companion)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    live_list = LiveList.find(params[:live_list_id])
+    @live_companion = live_list.live_companion
+    live_list.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
