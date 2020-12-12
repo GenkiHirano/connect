a024434ac7f10dca80638dba3ca943c6714efe05
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-12 20:59:11 +0900

    ライブ予定リストのリクエストテストを実装
    ライブ予定リストの保守性を担保する為

--- spec/requests/live_lists_spec.rb
+++ spec/requests/live_lists_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "ライブ予定リスト登録機能", type: :request do
   let(:user) { create(:user) }
+  let(:other_user) { create(:user) } 
+  let(:live_companion) { create(:live_companion, user: other_user) } 
 
   context "ライブ予定リスト一覧ページの表示" do
     context "ログインしている場合" do
@@ -17,6 +19,48 @@ RSpec.describe "ライブ予定リスト登録機能", type: :request do
       it "ログイン画面にリダイレクトすること" do
         get live_lists_path
         expect(response).to have_http_status "302"
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+
+  context "リスト登録/解除機能" do
+    context "ログインしている場合" do
+      before do
+        login_for_request(user)
+      end
+
+      it "ライブのリスト登録/解除ができること" do
+        expect {
+          post "/live_lists/#{live_companion.id}/create"
+        }.to change(other_user.live_lists, :count).by(1)
+        expect {
+          delete "/live_lists/#{LiveList.first.id}/destroy"
+        }.to change(other_user.live_lists, :count).by(-1)
+      end
+
+      it "ライブのAjaxによるリスト登録/解除ができること" do
+        expect {
+          post "/live_lists/#{live_companion.id}/create", xhr: true
+        }.to change(other_user.live_lists, :count).by(1)
+        expect {
+          delete "/live_lists/#{LiveList.first.id}/destroy", xhr: true
+        }.to change(other_user.live_lists, :count).by(-1)
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "リスト登録は実行できず、ログインページへリダイレクトすること" do
+        expect {
+          post "/live_lists/#{live_companion.id}/create"
+        }.not_to change(LiveList, :count)
+        expect(response).to redirect_to login_path
+      end
+
+      it "リスト解除は実行できず、ログインページへリダイレクトすること" do
+        expect {
+          delete "/live_lists/#{live_companion.id}/destroy"
+        }.not_to change(LiveList, :count)
         expect(response).to redirect_to login_path
       end
     end
