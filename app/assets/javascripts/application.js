a14f637828c08b9f55510a441423ed0dc061c8ef
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-12-25 14:07:20 +0900

    追加したbootstrap、css、jsファイルを読み込み用設定
    bootstrapファイルを反映させる為

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require rails-ujs
+//= require modernizr-2.6.2.min
+//= require activestorage
+//= require turbolinks
+// require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,9 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(
+  animate.css icomoon.css bootstrap.css flexslider.css \
+  style.css magnific-popup.css modernizr-2.6.2.min.js \
+  jquery.min.js jquery.easing.1.3.js bootstrap.min.js \
+  jquery.waypoints.min.js main.js jquery.flexslider-min.js custom.js)
