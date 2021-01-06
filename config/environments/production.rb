fa0a22dcace7d76a820f43689b221a0607623ca0
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-01-06 13:08:36 +0900

    config/environments/production.rbにconfig.assets.initialize_on_precompile = falseを追記
    herokuデプロイ時、Precompiling assets failed.エラーが発生する為

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+  config.assets.initialize_on_precompile = false
   config.log_formatter = ::Logger::Formatter.new
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
