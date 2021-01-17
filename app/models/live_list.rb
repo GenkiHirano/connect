0bcd0c1e3a33646d2b8c778bb37b849ae780b8e8
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-01-17 19:09:59 +0900

    ライブリストモデルの投稿表示順を指定する設定を削除
    エラーの原因を切り分ける為

--- app/models/live_list.rb
+++ app/models/live_list.rb
@@ -1,7 +1,6 @@
 class LiveList < ApplicationRecord
   belongs_to :user
   belongs_to :live_companion
-  default_scope -> { order(schedule: :desc) }
   validates :user_id, presence: true
   validates :live_companion_id, presence: true
   validates :from_user_id, presence: true
