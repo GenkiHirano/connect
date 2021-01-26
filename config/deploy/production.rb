6ed3ce14e971fe6dac372233fe73e743265f5897
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-01-27 00:54:00 +0900

    config/deploy/production.rbを修正
    デプロイ時にSSHKit::Runner::ExecuteErrorが発生した為

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
-server '18.179.117.162', user: 'GenkiHirano', roles: %w{app db web} 
-
-set :ssh_options, keys: '~/.ssh/connect-genkihirano.pem' 
+set :ssh_options, {
+  port: 18.179.117.162,
+  keys: [File.expand_path('~/.ssh/connect-genkihirano_aws_rsa')],
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
