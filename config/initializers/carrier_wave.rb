fe5236c6b827a68b12f86306480cdb142f213216
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-02-03 23:10:40 +0900

    config/credentials.yml.encを編集
    画像投稿に使うS3のアクセスキー、シークレットアクセスキーを保管する為

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider => 'AWS',
-      :region => ENV['S3_REGION'],
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      provider: "AWS",
+      region: "ap-northeast-1",
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
     }
-    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory = 'connect-genkihirano'
   end
 end
