dacd1b62ff84dfc084c53aa6ad10145da312f9d4
Genki Hirano <genki_rad@ezweb.ne.jp>
2021-04-08 17:09:16 +0900

    production.rb修正
    PCが故障した影響で全データを消去した為、AWSデプロイ再設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "54.168.46.152", user: "GenkiHirano", roles: %w(app db web)
+server "35.73.135.7", user: "GenkiHirano", roles: %w(app db web)
 
 set :ssh_options, {
   keys: %w(~/.ssh/connect_genkihirano.pem),
